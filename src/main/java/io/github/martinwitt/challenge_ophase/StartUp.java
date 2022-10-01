6232aa8249238dd1275cbfbe13044f7a307d9fc1
Martin Wittlinger <wittlinger.martin@gmail.com>
2022-10-01 16:39:55 +0200

    add first challenge

--- src/main/java/io/github/martinwitt/challenge_ophase/StartUp.java
+++ src/main/java/io/github/martinwitt/challenge_ophase/StartUp.java
@@ -1,13 +1,20 @@
 package io.github.martinwitt.challenge_ophase;
 
 import io.quarkus.runtime.StartupEvent;
+import java.util.List;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @ApplicationScoped
 public class StartUp {
 
+    @ConfigProperty(name = "first.challenge.name")
+    String firstChallengeName;
+
     void onStart(@Observes StartupEvent ev) {
         System.out.println("The application is starting...");
+        Challenge challenge = new Challenge(firstChallengeName, "aaa", "cccc", List.of("lol"), List.of(), false, "bbb");
+        challenge.persistOrUpdate();
     }
 }
